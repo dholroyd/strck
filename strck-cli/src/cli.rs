c3d2980f7c306084b5c4abd062c222679a2de068
David Holroyd <dave@badgers-in-foil.co.uk>
2019-09-26 13:23:55 +0100

    Improved help text

--- strck-cli/src/cli.rs
+++ strck-cli/src/cli.rs
@@ -4,8 +4,9 @@ use reqwest::Url;
 #[derive(StructOpt)]
 #[structopt(name = "strck", about = "Media stream check")]
 pub enum Strck {
-    #[structopt(name = "hls", about = "Check an HTTP Live Streaming master manifest")]
+    #[structopt(name = "hls", about = "Check an 'HTTP Live Streaming' endpoint")]
     Hls {
+        #[structopt(help = "HLS 'Master Manifest' URL")]
         manifest: Url,
     }
 }
